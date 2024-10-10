cd68d399c60b8ca7256bfa38f9135ad7e699d5be
abraham241 <huguesmboundou@gmail.com>
2024-10-10 17:26:23 +0100

    resposive section choix

--- app/components/Choix.tsx
+++ app/components/Choix.tsx
@@ -1,19 +1,80 @@
 import React from "react";
 
+
 export default function choix() {
   return (
     <>
-      <div>
-        <div className="text-center">
-          <h1>
+      <div className="md:px-20 py-10 px-10">
+        {/* Titre */}
+        <div className="text-center py-10">
+          <h1 className="text-4xl font-bold py-4">
             Pourquoi choisir recru<span>Pay</span> pour votre entreprise
           </h1>
           <p>
             Avec des fonctionnalités d’automatisation puissantes, telles que le
-            tri <br /> automatique des CV et la gestion des formations en ligne,
-            RecruPay vous <br /> offre des tableaux de bord et rapports
-            détaillés
+            tri <br />
+            automatique des CV et la gestion des formations en ligne, RecruPay
+            vous <br />
+            offre des tableaux de bord et rapports détaillés
           </p>
+        </div>
+
+        {/* Section cards */}
+        <div className="flex flex-col lg:flex-row lg:justify-between gap-5">
+          <div className="bg-gray-300 w-full lg:w-1/3 p-10 rounded-md">
+            <div className="text-3xl font-bold">
+              <p>01</p>
+            </div>
+            <div className="text-xl font-bold py-2">
+              <h1>Automatisation des processus</h1>
+            </div>
+            <div>
+              <p>
+                Le logiciel vous permet de simplifier et d’automatiser les{" "}
+                <br />
+                tâches chronophages, comme le tri des candidatures, la <br />
+                planification des entretiens et le suivi des candidats. Cela{" "}
+                <br />
+                améliore la productivité et permet à l'équipe RH de se <br />
+                concentrer sur les tâches à plus forte valeur ajoutée.
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-gray-300 w-full lg:w-1/3 p-10 rounded-md">
+            <div className="text-3xl font-bold">
+              <p>02</p>
+            </div>
+            <div className="text-xl font-bold py-2">
+              <h1>Gestion des formations</h1>
+            </div>
+            <div>
+              <p>
+                Créez des programmes de formation personnalisés, adaptés aux{" "}
+                <br />
+                besoins spécifiques de votre entreprise. Suivez la progression{" "}
+                <br />
+                de chaque employé en temps réel avec des rapports détaillés.
+              </p>
+            </div>
+          </div>
+
+          <div className="bg-gray-300 w-full lg:w-1/3 p-10 rounded-md">
+            <div className="text-3xl font-bold">
+              <p>03</p>
+            </div>
+            <div className="text-xl font-bold py-2">
+              <h1>Collaboration améliorée</h1>
+            </div>
+            <div>
+              <p>
+                Favorisez la collaboration entre équipes avec des outils <br />
+                intégrés comme la messagerie instantanée et le partage de <br />
+                ressources pour une communication fluide au sein de
+                l'entreprise.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
